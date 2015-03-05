da96e6fb9248004c295540f5e29db58304741e13
kollors <kollors@mail.ru>
2015-03-05 15:48:56 +0300

    adding favicon

--- index.php
+++ index.php
@@ -13,6 +13,7 @@
     <title>НПО БОНВИО</title>
 
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="img/galk-white.png" type="image/x-icon">
 
     <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,700' rel='stylesheet' type='text/css'>
 
